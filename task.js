03ea028bf0cd022a818915b12605b5f9859b9806
davych <davych@163.com>
2022-08-11 14:25:34 +0800

    a commit a day keeps your girlfriend away Sunday, January 1, 1995 12:00 AM

--- task.js
+++ task.js
@@ -1,11 +1,11 @@
-import { execa } from 'execa';
+import {execa} from 'execa';
 import moment from 'moment';
 import fs from 'fs';
 
 
 const fileOP = () => {
   return new Promise((resolve, reject) => {
-    fs.readFile("count.txt", function (err, buf) {
+    fs.readFile("count.txt", function(err, buf) {
       const count = parseInt(buf.toString());
       fs.writeFile("count.txt", `${count + 1}`, (err) => {
         console.log("Successfully Written to File.");
@@ -18,35 +18,19 @@ const fileOP = () => {
 await execa('git', ['config', '--local', 'user.email', "davych@163.com"]);
 await execa('git', ['config', '--local', 'user.name', "davych"]);
 (async () => {
-  const list = [2022];
-  let cal = moment(`2022-01-01`)
+  const list = [1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022,2023,2024];
+ list.forEach(async (y) => {
+  let cal = moment(`${y}-01-01`);
   let days = 360;
-  const r = [
-    '01-30',
-    '01-30',
-    '02-01',
-    '02-02',
-    '02-03',
-    '02-04',
-    '02-05',
-    '01-26',
-    '01-19',
-    '01-20',
-    '01-21',
-    '01-22',
-    '01-29'
-  ]
   for (let i = 0; i < days; i++) {
-    let date = cal.format('MM-DD');
-    console.log(date);
-    if (r.includes(date)) {
-      await fileOP();
-      await execa('git', ['add', '.']);
-      await execa('git', ['commit', '-m', `a commit a day keeps your girlfriend away ${date}`]);
-      await execa('git', ['push', '-f']);
-      await execa('git', ['commit', '--amend', '--no-edit', "--date", date]);
-      await execa('git', ['push', '-f']);
-    }
+    let date = cal.format('LLLL');
+    await fileOP();
+    await execa('git', ['add', '.']);
+    await execa('git', ['commit', '-m', `a commit a day keeps your girlfriend away ${date}`]);
+    await execa('git', ['push', '-f']);
+    await execa('git', ['commit', '--amend', '--no-edit', "--date", date]);
+    await execa('git', ['push', '-f']);
     cal.add(1, 'days');
   }
+  })
 })()
